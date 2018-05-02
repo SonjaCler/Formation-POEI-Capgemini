c4036f9ba460cbb9351f5564ba534580c28beb24
xhackax47 <xhackax47@gmail.com>
2018-05-02 12:16:11 +0200

    Methode delete testée sur JSP

--- smooss/src/main/java/com/aplose/smooss/services/EventService.java
+++ smooss/src/main/java/com/aplose/smooss/services/EventService.java
@@ -42,12 +42,13 @@ public class EventService {
 		return evt;
 	}
 	
-	public void modify(Event evt) {
+	public Event modify(Event evt) {
 		if (!JPASingleton.getInstance().getEntityManager().getTransaction().isActive()) {
 			JPASingleton.getInstance().getEntityManager().getTransaction().begin();
 		}
 		JPASingleton.getInstance().getEntityManager().merge(evt);
 		JPASingleton.getInstance().getEntityManager().getTransaction().commit();
+		return evt;
 	}
 
 	public List<Event> findEventsByUser(User user) {
